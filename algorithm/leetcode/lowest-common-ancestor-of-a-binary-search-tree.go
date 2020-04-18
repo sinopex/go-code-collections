1f49e60846fd9ebd73abfb98ddb97ec7382ec4bb
sinopex <wsj6563@gmail.com>
2020-04-18 23:12:48 +0800

    pref: lowest-common-ancestor-of-a-binary-search-tree.go

--- algorithm/leetcode/lowest-common-ancestor-of-a-binary-search-tree.go
+++ algorithm/leetcode/lowest-common-ancestor-of-a-binary-search-tree.go
@@ -3,28 +3,13 @@
 package leetcode
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-	// p小，q大
-	if p.Val > q.Val {
-		p, q = q, p
-	}
-	return _lowestCommonAncestor(root, p, q)
-}
-
-func _lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-	// p,q 在root的两边，那么顶点就是最近公共祖先
-	if root.Val >= p.Val && root.Val <= q.Val {
-		return root
+	if p.Val < root.Val && q.Val < root.Val {
+		return lowestCommonAncestor(root.Left, p, q)
 	}
 
-	// root在右侧，则往左下沉
-	if root.Val > p.Val && root.Val > q.Val {
-		return _lowestCommonAncestor(root.Left, p, q)
+	if p.Val > root.Val && q.Val > root.Val {
+		return lowestCommonAncestor(root.Right, p, q)
 	}
 
-	// root在左侧，则往右下沉
-	if root.Val < p.Val && root.Val < q.Val {
-		return _lowestCommonAncestor(root.Right, p, q)
-	}
-
-	return nil
+	return root
 }
