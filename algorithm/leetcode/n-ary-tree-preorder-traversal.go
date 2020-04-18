24feed619100ccb53df8b8693893be0b2c1c7620
sinopex <wsj6563@gmail.com>
2020-04-18 12:11:07 +0800

    feat: n-ary-tree-preorder-traversal.go

---
+++ algorithm/leetcode/n-ary-tree-preorder-traversal.go
@@ -0,0 +1,39 @@
+// 589 N叉树的前序遍历
+// https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/
+package leetcode
+
+func preorder(root *Node) []int {
+	if root == nil {
+		return []int{}
+	}
+
+	res := []int{root.Val}
+
+	for _, node := range root.Children {
+		res = append(res, preorder(node)...)
+	}
+
+	return res
+}
+
+func preorderBFS(root *Node) []int {
+	if root == nil {
+		return []int{}
+	}
+
+	res := make([]int, 0)
+	queues := []*Node{root}
+	for len(queues) > 0 {
+		node := queues[0]
+		queues = queues[1:]
+		res = append(res, node.Val)
+
+		var tmpNode []*Node
+		for _, node := range node.Children {
+			tmpNode = append(tmpNode, node)
+		}
+		queues = append(tmpNode, queues...)
+	}
+
+	return res
+}
