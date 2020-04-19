19392836baa0fa92b876c7589453eb584faa07e1
sinopex <wsj6563@gmail.com>
2020-04-19 22:59:15 +0800

    feat: leaf-similar-trees.go

---
+++ algorithm/leetcode/leaf-similar-trees.go
@@ -0,0 +1,34 @@
+// 872 叶子相似的树
+// https://leetcode-cn.com/problems/leaf-similar-trees/
+package leetcode
+
+func leafSimilar(root1 *TreeNode, root2 *TreeNode) bool {
+	ret1 := make([]int, 0)
+	ret2 := make([]int, 0)
+	_leafSimilar(root1, &ret1)
+	_leafSimilar(root2, &ret2)
+
+	if len(ret1) != len(ret2) {
+		return false
+	}
+
+	for k, v := range ret1 {
+		if ret2[k] != v {
+			return false
+		}
+	}
+
+	return true
+}
+func _leafSimilar(root *TreeNode, ret *[]int) {
+	if root == nil {
+		return
+	}
+	if root.Left == nil && root.Right == nil {
+		*ret = append(*ret, root.Val)
+		return
+	}
+
+	_leafSimilar(root.Left, ret)
+	_leafSimilar(root.Right, ret)
+}
