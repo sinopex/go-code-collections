14fe69d0a940d74ab93f021ad6f6a5963bf34c29
sinopex <wsj6563@gmail.com>
2020-04-19 18:26:34 +0800

    perf: trim-a-binary-search-tree.go

--- algorithm/leetcode/trim-a-binary-search-tree.go
+++ algorithm/leetcode/trim-a-binary-search-tree.go
@@ -48,3 +48,21 @@ func trimBST(root *TreeNode, L int, R int) *TreeNode {
 
 	return root
 }
+
+func trimBSTDFS(root *TreeNode, L int, R int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Val > R {
+		return trimBST(root.Left, L, R)
+	}
+
+	if root.Val < L {
+		return trimBST(root.Right, L, R)
+	}
+
+	root.Left = trimBST(root.Left, L, R)
+	root.Right = trimBST(root.Right, L, R)
+	return root
+}
