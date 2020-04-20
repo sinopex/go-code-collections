a9b8861bdbeb81cf002aeadff8c13aec7d890c92
sinopex <wsj6563@gmail.com>
2020-04-20 23:09:03 +0800

    feat: two-sum-iv-input-is-a-bst.go

---
+++ algorithm/leetcode/two-sum-iv-input-is-a-bst.go
@@ -0,0 +1,21 @@
+// 653 两数之和IV - 输入 BST
+// https://leetcode-cn.com/problems/two-sum-iv-input-is-a-bst/
+package leetcode
+
+func findTarget(root *TreeNode, k int) bool {
+	var history = make(map[int]struct{})
+	return _findTarget(root, k, &history)
+}
+func _findTarget(root *TreeNode, k int, history *map[int]struct{}) bool {
+	if root == nil {
+		return false
+	}
+
+	// 已经找到，终止继续查找
+	if _, ok := (*history)[k-root.Val]; ok {
+		return true
+	}
+	(*history)[root.Val] = struct{}{}
+
+	return _findTarget(root.Left, k, history) || _findTarget(root.Right, k, history)
+}
