d44aab4997751d12c44ac4ec0fd094827ba63d12
sinopex <wsj6563@gmail.com>
2020-04-21 23:26:05 +0800

    perf: path-sum-iii.go

--- algorithm/leetcode/path-sum-iii.go
+++ algorithm/leetcode/path-sum-iii.go
@@ -3,25 +3,46 @@
 package leetcode
 
 func pathSum(root *TreeNode, sum int) int {
-	if root == nil {
-		return 0
-	}
-	return PathFrom(root, sum) + pathSum(root.Left, sum) + pathSum(root.Right, sum)
+	var c int
+	var v = make(map[int]int)
+	v[0] = 1
+	_pathSum(root, sum, 0, &c, v)
+	return c
 }
 
-func PathFrom(root *TreeNode, sum int) int {
+func _pathSum(root *TreeNode, sum, ps int, c *int, v map[int]int) {
 	if root == nil {
-		return 0
+		return
 	}
 
-	cnt := 0
-
-	if root.Val == sum {
-		cnt++
-	}
-
-	cnt += PathFrom(root.Left, sum-root.Val)
-	cnt += PathFrom(root.Right, sum-root.Val)
-
-	return cnt
+	ps += root.Val
+	*c += v[ps-sum]
+	v[ps] += 1
+	_pathSum(root.Left, sum, ps, c, v)
+	_pathSum(root.Right, sum, ps, c, v)
+	v[ps] -= 1
 }
+
+// func pathSum(root *TreeNode, sum int) int {
+// 	if root == nil {
+// 		return 0
+// 	}
+// 	return PathFrom(root, sum) + pathSum(root.Left, sum) + pathSum(root.Right, sum)
+// }
+//
+// func PathFrom(root *TreeNode, sum int) int {
+// 	if root == nil {
+// 		return 0
+// 	}
+//
+// 	cnt := 0
+//
+// 	if root.Val == sum {
+// 		cnt++
+// 	}
+//
+// 	cnt += PathFrom(root.Left, sum-root.Val)
+// 	cnt += PathFrom(root.Right, sum-root.Val)
+//
+// 	return cnt
+// }
