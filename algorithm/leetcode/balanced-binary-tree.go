01672a639721756f225929473fd6a9490589cf59
sinopex <wsj6563@gmail.com>
2020-04-22 22:31:22 +0800

    feat: balanced-binary-tree.go

---
+++ algorithm/leetcode/balanced-binary-tree.go
@@ -0,0 +1,31 @@
+// 110 平衡二叉树
+// https://leetcode-cn.com/problems/balanced-binary-tree/submissions/
+package leetcode
+
+func isBalanced(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	_, b := _isBalancedDepth(root)
+	return b
+}
+
+func _isBalancedDepth(root *TreeNode) (int, bool) {
+	if root == nil {
+		return 0, true
+	}
+
+	ld, lt := _isBalancedDepth(root.Left)
+	rd, rt := _isBalancedDepth(root.Right)
+
+	depth := ld
+	x := ld - rd
+	if rd > ld {
+		depth = rd
+		x = rd - ld
+	}
+	depth++
+
+	return depth, lt && rt && x <= 1
+}
