cd49d30e4aad38f24f798c5d586ed0e6ade9945e
sinopex <wsj6563@gmail.com>
2020-04-24 00:18:11 +0800

    perf: second-minimum-node-in-a-binary-tree.go

--- algorithm/leetcode/second-minimum-node-in-a-binary-tree.go
+++ algorithm/leetcode/second-minimum-node-in-a-binary-tree.go
@@ -3,34 +3,25 @@
 package leetcode
 
 func findSecondMinimumValue(root *TreeNode) int {
-	var data = make([]int, 0)
-
-	_findSecondMinimumValue(root, &data)
-
-	if len(data) == 2 {
-		return data[1]
-	}
-
-	return -1
+	var n int = -1
+	_findSecondMinimumValue(root, root.Val, &n)
+	return n
 }
-func _findSecondMinimumValue(root *TreeNode, data *[]int) {
+func _findSecondMinimumValue(root *TreeNode, b int, n *int) {
 	if root == nil {
 		return
 	}
 
-	n := len(*data)
-	if n == 0 {
-		*data = append(*data, root.Val)
-	} else if n == 1 {
-		if root.Val != (*data)[0] {
-			*data = append(*data, root.Val)
-		}
-	} else {
-		// 第二小的需要判断
-		if root.Val != (*data)[0] && (*data)[1] > root.Val {
-			(*data)[1] = root.Val
-		}
+	// 还没赋值
+	if *n == -1 && root.Val != b {
+		*n = root.Val
 	}
-	_findSecondMinimumValue(root.Left, data)
-	_findSecondMinimumValue(root.Right, data)
+
+	// 找到一个与根节点不一样的，且比当前第二小还小的数值
+	if *n > root.Val && root.Val != b {
+		*n = root.Val
+	}
+
+	_findSecondMinimumValue(root.Left, b, n)
+	_findSecondMinimumValue(root.Right, b, n)
 }
