772b3cc7eabd32abd09d57da49cba0e0947a60b1
sinopex <wsj6563@gmail.com>
2020-04-27 23:59:26 +0800

    feat: generate-parentheses.go

---
+++ algorithm/leetcode/generate-parentheses.go
@@ -0,0 +1,29 @@
+// 22 括号生成
+// https://leetcode-cn.com/problems/generate-parentheses/
+package leetcode
+
+import (
+	"fmt"
+)
+
+func generateParenthesis(n int) []string {
+	var ret []string
+	_generateParenthesis(n, n, "", &ret)
+	return ret
+}
+
+func _generateParenthesis(left, right int, p string, ret *[]string) {
+	if left == 0 && right == 0 {
+		fmt.Println("p", p)
+		*ret = append(*ret, p)
+		return
+	}
+
+	if left > 0 {
+		_generateParenthesis(left-1, right, p+"(", ret)
+	}
+
+	if right > left && right > 0 {
+		_generateParenthesis(left, right-1, p+")", ret)
+	}
+}
