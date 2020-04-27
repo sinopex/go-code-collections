56b91908681a579d8c71d755591a48046a0b9a1c
sinopex <wsj6563@gmail.com>
2020-04-27 22:20:26 +0800

    feat: split-a-string-in-balanced-strings.go

---
+++ algorithm/leetcode/split-a-string-in-balanced-strings.go
@@ -0,0 +1,22 @@
+// 1221 分割平衡字符串
+// https://leetcode-cn.com/problems/split-a-string-in-balanced-strings/
+package leetcode
+
+func balancedStringSplit(s string) int {
+	ln, rn, cnt := 0, 0, 0
+	for _, c := range s {
+		if c == 'L' {
+			ln++
+		}
+		if c == 'R' {
+			rn++
+		}
+
+		if ln == rn {
+			cnt++
+			ln, rn = 0, 0
+		}
+	}
+
+	return cnt
+}
