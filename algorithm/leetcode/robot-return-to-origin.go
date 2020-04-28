d329988dcad91da44d9730113d4054b0fd184f43
sinopex <wsj6563@gmail.com>
2020-04-28 23:46:44 +0800

    feat: robot-return-to-origin.go

---
+++ algorithm/leetcode/robot-return-to-origin.go
@@ -0,0 +1,17 @@
+// 657 机器人能否回到原点
+// https://leetcode-cn.com/problems/robot-return-to-origin/
+package leetcode
+
+func judgeCircle(moves string) bool {
+	var set = map[rune]int{
+		'U': 0,
+		'L': 0,
+		'R': 0,
+		'D': 0,
+	}
+	for _, s := range moves {
+		set[s]++
+	}
+
+	return (set['U']-set['D'] == 0) && (set['L']-set['R'] == 0)
+}
