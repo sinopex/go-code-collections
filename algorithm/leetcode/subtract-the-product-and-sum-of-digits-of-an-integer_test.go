2ffd2e897066b5f0884643fc8265aa3722fab274
sinopex <wsj6563@gmail.com>
2020-04-28 22:43:33 +0800

    feat: subtract-the-product-and-sum-of-digits-of-an-integer.go

---
+++ algorithm/leetcode/subtract-the-product-and-sum-of-digits-of-an-integer_test.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+func Test_subtractProductAndSum(t *testing.T) {
+	tests := []struct {
+		name string
+		n    int
+		want int
+	}{
+		{name: "test1", n: 234, want: 15},
+		{name: "test2", n: 4421, want: 21},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := subtractProductAndSum(tt.n); got != tt.want {
+				t.Errorf("subtractProductAndSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
