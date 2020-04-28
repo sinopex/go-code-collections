54daf737e2bfaf51786d18c0aece0eddd1107502
sinopex <wsj6563@gmail.com>
2020-04-28 22:06:24 +0800

    feat: zuo-xuan-zhuan-zi-fu-chuan-lco.go

---
+++ algorithm/leetcode/zuo-xuan-zhuan-zi-fu-chuan-lco.go
@@ -0,0 +1,20 @@
+// 面试题58-II 左旋转字符串
+// https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof/
+package leetcode
+
+import (
+	"strings"
+)
+
+func reverseLeftWords(s string, n int) string {
+	l := len(s)
+	if l == 0 {
+		return ""
+	}
+	n = n % l
+
+	var sb strings.Builder
+	sb.WriteString(s[n:])
+	sb.WriteString(s[0:n])
+	return sb.String()
+}
---
+++ algorithm/leetcode/zuo-xuan-zhuan-zi-fu-chuan-lco_test.go
@@ -0,0 +1,29 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+func Test_reverseLeftWords(t *testing.T) {
+	type args struct {
+		s string
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "test1", args: args{s: "", n: 2}, want: ""},
+		{name: "test1", args: args{s: "abcdefg", n: 2}, want: "cdefgab"},
+		{name: "test2", args: args{s: "abcdefg", n: 9}, want: "cdefgab"},
+		{name: "test3", args: args{s: "lrloseumgh", n: 6}, want: "umghlrlose"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseLeftWords(tt.args.s, tt.args.n); got != tt.want {
+				t.Errorf("reverseLeftWords() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
