a6b32fe0048c78b1229424c871e671611ef1182a
sinopex <wsj6563@gmail.com>
2020-05-01 00:39:21 +0800

    feat: unique-email-addresses.go

---
+++ algorithm/leetcode/unique-email-addresses_test.go
@@ -0,0 +1,38 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+func Test_numUniqueEmails(t *testing.T) {
+	type args struct {
+		emails []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test1",
+			args: args{
+				emails: []string{"test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"},
+			},
+			want: 2,
+		},
+		{
+			name: "test2",
+			args: args{
+				emails: []string{"123@qq.com", "1.2.3@qq.com", "1..2..3+wong@qq.com"},
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numUniqueEmails(tt.args.emails); got != tt.want {
+				t.Errorf("numUniqueEmails() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
