579f1a295da973b6579c84fa9e03ed5b8713b238
sinopex <wsj6563@gmail.com>
2020-04-30 23:43:40 +0800

    feat: groups-of-special-equivalent-strings.go

---
+++ algorithm/leetcode/groups-of-special-equivalent-strings_test.go
@@ -0,0 +1,52 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+func Test_numSpecialEquivGroups(t *testing.T) {
+	type args struct {
+		A []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test",
+			args: args{
+				A: []string{"a", "b", "c", "a", "c", "c"},
+			},
+			want: 3,
+		},
+		{
+			name: "test",
+			args: args{
+				A: []string{"aa", "bb", "ab", "ba"},
+			},
+			want: 4,
+		},
+		{
+			name: "test",
+			args: args{
+				A: []string{"abc", "acb", "bac", "bca", "cab", "cba"},
+			},
+			want: 3,
+		},
+		{
+			name: "test",
+			args: args{
+				A: []string{"abcd", "cdab", "cbad", "xyzz", "zzxy", "zzyx"},
+			},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numSpecialEquivGroups(tt.args.A); got != tt.want {
+				t.Errorf("numSpecialEquivGroups() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
