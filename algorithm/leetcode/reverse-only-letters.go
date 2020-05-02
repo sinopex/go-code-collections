2ab7c16d829d8be5c5fb482d8260fe21f20f260d
sinopex <wsj6563@gmail.com>
2020-05-02 17:50:49 +0800

    perf: reverse-only-letters.go

---
+++ algorithm/leetcode/reverse-only-letters.go
@@ -0,0 +1,34 @@
+// 917. 仅仅反转字母
+// https://leetcode-cn.com/problems/reverse-only-letters/
+package leetcode
+
+import (
+	"bytes"
+)
+
+func reverseOnlyLetters(S string) string {
+	var buf bytes.Buffer
+	var ret bytes.Buffer
+	n := len(S)
+
+	for i := n - 1; i >= 0; i-- {
+		if isValidAlphabet(S[i]) {
+			buf.WriteByte(S[i])
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		if !isValidAlphabet(S[i]) {
+			ret.WriteByte(S[i])
+		} else {
+			b, _ := buf.ReadByte()
+			ret.WriteByte(b)
+		}
+	}
+
+	return ret.String()
+}
+
+func isValidAlphabet(b byte) bool {
+	return (b >= 'A' && b <= 'Z') || (b >= 'a' && b <= 'z')
+}
