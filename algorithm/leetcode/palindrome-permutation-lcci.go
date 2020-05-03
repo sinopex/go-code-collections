42757f6aadacfe028aa9aefcbe27f0a6a51f6c5e
sinopex <wsj6563@gmail.com>
2020-05-03 14:43:17 +0800

    feat: palindrome-permutation-lcci.go

---
+++ algorithm/leetcode/palindrome-permutation-lcci.go
@@ -0,0 +1,27 @@
+// 面试题 01.04. 回文排列
+// https://leetcode-cn.com/problems/palindrome-permutation-lcci/
+package leetcode
+
+func canPermutePalindrome(s string) bool {
+	var buf = []byte(s)
+	var dict = make(map[byte]int)
+	var oddNum = 0
+	for _, v := range buf {
+		if dict[v] > 0 {
+			dict[v]--
+		} else {
+			dict[v]++
+		}
+	}
+
+	for _, v := range dict {
+		if v == 1 {
+			oddNum++
+		}
+		if oddNum >= 2 {
+			return false
+		}
+	}
+
+	return true
+}
