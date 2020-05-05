903f2546de6deb35ebafd7bfb29a61f49ebb4f52
sinopex <wsj6563@gmail.com>
2020-05-05 16:35:15 +0800

    feat: repeated-substring-pattern.go

---
+++ algorithm/leetcode/repeated-substring-pattern.go
@@ -0,0 +1,23 @@
+// 459. 重复的子字符串
+// https://leetcode-cn.com/problems/repeated-substring-pattern/
+package leetcode
+
+import (
+	"strings"
+)
+
+func repeatedSubstringPattern(s string) bool {
+	return strings.Contains((s + s)[1:len(s)*2-1], s)
+}
+
+// func repeatedSubstringPattern(s string) bool {
+// 	n := len(s)
+// 	for i := 1; i <= n/2; i++ {
+// 		if n%i == 0 {
+// 			if strings.Repeat(s[0:i], n/i) == s {
+// 				return true
+// 			}
+// 		}
+// 	}
+// 	return false
+// }
