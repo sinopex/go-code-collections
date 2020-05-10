2aee2cfb11e76a1aaa7f5cabe2e6ca9fbc9094da
sinopex <wsj6563@gmail.com>
2020-05-10 11:03:04 +0800

    feat: length-of-last-word.go

---
+++ algorithm/leetcode/length-of-last-word.go
@@ -0,0 +1,26 @@
+// 58. 最后一个单词的长度
+// https://leetcode-cn.com/problems/length-of-last-word/
+package leetcode
+
+func lengthOfLastWord(s string) int {
+	n := len(s)
+	if n == 0 {
+		return 0
+	}
+	j := n - 1
+	// 去掉末位的空格
+	for j >= 0 && s[j] == ' ' {
+		j--
+	}
+
+	count := 0
+	for j >= 0 {
+		if s[j] == ' ' {
+			return count
+		}
+		count++
+		j--
+	}
+
+	return count
+}
