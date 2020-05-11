a5e1de405e2333a2149ce6b5c78470b07b9ae42d
sinopex <wsj6563@gmail.com>
2020-05-11 22:17:00 +0800

    feat: buddy-strings.go

---
+++ algorithm/leetcode/buddy-strings.go
@@ -0,0 +1,51 @@
+// 859. 亲密字符串
+// https://leetcode-cn.com/problems/buddy-strings/
+package leetcode
+
+func buddyStrings(A string, B string) bool {
+	na, nb := len(A), len(B)
+	if na != nb {
+		return false
+	}
+
+	if na < 2 {
+		return false
+	}
+
+	// 如果A、B相等，则查询是否有字符超过2个
+	if A == B {
+		chars := [26]int{}
+		for _, v := range A {
+			chars[v-'a']++
+			if chars[v-'a'] == 2 {
+				return true
+			}
+		}
+		return false
+	}
+
+	// 不相等，则需要置换位置
+	prev := -1
+	replaced := false
+	for i := 0; i < na; i++ {
+		if A[i] != B[i] {
+			if replaced == true {
+				return false
+			}
+			// 第一个位置
+			if prev == -1 {
+				prev = i
+			} else {
+				// 第二个位置，直接比较
+				if A[prev] == B[i] && A[i] == B[prev] {
+					replaced = true
+				} else {
+					return false
+				}
+			}
+		}
+
+	}
+
+	return replaced
+}
