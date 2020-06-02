c1597178132184fe9791473a850571e99e3e85a4
sinopex <wsj6563@gmail.com>
2020-06-02 23:58:12 +0800

    feat: shift-2d-grid.go

--- algorithm/leetcode/array/shift-2d-grid.go
+++ algorithm/leetcode/array/shift-2d-grid.go
@@ -4,6 +4,37 @@ package array
 
 func shiftGrid(grid [][]int, k int) [][]int {
 	m, n := len(grid), len(grid[0])
+	k %= m * n
+	result := make([][]int, m)
+	for i, _ := range result {
+		result[i] = make([]int, n)
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			pos := i*n + j + k
+			result[pos/n%m][pos%n] = grid[i][j]
+		}
+	}
+
+	return result
+}
+
+func shiftGrid3(grid [][]int, k int) [][]int {
+	m, n := len(grid), len(grid[0])
+	k %= m * n
+	for i := 0; i < k; i++ {
+		previous := grid[m-1][n-1]
+		for j, _ := range grid {
+			for k, _ := range grid[j] {
+				previous, grid[j][k] = grid[j][k], previous
+			}
+		}
+	}
+	return grid
+}
+
+func shiftGrid2(grid [][]int, k int) [][]int {
+	m, n := len(grid), len(grid[0])
 	all := make([]int, m*n)
 	for index, row := range grid {
 		copy(all[index*n:(index+1)*n], row)
